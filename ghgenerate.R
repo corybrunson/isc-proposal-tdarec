ce3c67165204e6bec99009b1c511f33657f71380
Jason Cory Brunson <cornelioid@gmail.com>
2024-01-12 17:47:08 -0500

    remove files + ignore output + draft & itemize content

--- ghgenerate.R
+++ ghgenerate.R
@@ -1,6 +1,9 @@
-proposal<-"Boilerplate ISC Proposal"
-proposal.file<-"isc-proposal.Rmd"
-author<-"Steph Locke"
+proposal <- paste0(
+  "Integrating topological data analytic tools ",
+  "into the tidy data analysis and machine learning ecosystems."
+)
+proposal.file <- "tdarec-proposal.Rmd"
+author <- "Jason Cory Brunson"
 
 rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
