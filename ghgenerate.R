87d2ff9cbe339e34b42ea8f7549631152c8abe19
Jason Cory Brunson <cornelioid@gmail.com>
2024-03-31 19:42:13 -0400

    expand all notes to text + reduce length

--- ghgenerate.R
+++ ghgenerate.R
@@ -2,8 +2,11 @@ proposal <- paste0(
   "Integrating topological data analytic tools ",
   "into the tidy data analysis and machine learning ecosystems."
 )
+proposal <- paste0(
+  "Modular, interoperable, and extensible topological data analysis in R."
+)
 proposal.file <- "tdarec-proposal.Rmd"
-author <- "Jason Cory Brunson"
+author <- "Jason Cory Brunson and Aymeric Stamm"
 
 rmarkdown::render(proposal.file, output_format="html_document",
                   output_dir="out", quiet=TRUE)
